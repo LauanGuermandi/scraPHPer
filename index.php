d647b80e19a67e943b2b3aea2a7ca41c45898b65
Lauan guermandi <lauan@d4rk.localdomain>
2018-02-19 01:01:01 -0300

    improve validation

--- index.php
+++ index.php
@@ -2,30 +2,29 @@
 
 require_once 'functions.php';
 
+$ch                 = curl_init();
+$arrLinks           = [];
+$arrLinksError      = [];
+$pageLinks          = [];
 
-$ch = curl_init();
-$arrLinks = [];
-$arrLinksError = [];
-$pageLinks = [];
-
-$url = readline("Insert the initial URL to test: ");
-$limit = readline("Insert the limit of pages to test: ");
-$pathJsonLinks = readline("Insert the path to json links file(Optional): ");
+$url                = readline("Insert the initial URL to test: ");
+$limit              = readline("Insert the limit of pages to test: ");
+$pathJsonLinks      = readline("Insert the path to json links file(Optional): ");
 
 if( file_exists( $pathJsonLinks ) ){
-    $jsonLinks = file_get_contents( $pathJsonLinks );
-    $arrLinks = json_decode( $jsonLinks, true, JSON_UNNESCAPED_SLASHES );
+    $jsonLinks      = file_get_contents( $pathJsonLinks );
+    $arrLinks       = json_decode( $jsonLinks, true, JSON_UNNESCAPED_SLASHES );
 
     testingLinks( $arrLinks );
 
-    saveLinksJson( $arrLinks, "LinksError" );
+    if( !is_null( $arrLinksError ) ) saveLinksJson( $arrLinksError, "LinksError" );
 }else{
 
-    $result = sendRequest( $url );
+    $result         = sendRequest( $url );
     
     for($i=0; $result != false; $i++){
-        $arrLinks = array_merge( getLinks( $result ), $arrLinks );
-        $result = sendRequest( $arrLinks[ $i ] );
+        $arrLinks   = array_merge( getLinks( $result ), $arrLinks );
+        $result     = sendRequest( $arrLinks[ $i ] );
     }
     
     testingLinks( $arrLinks );
